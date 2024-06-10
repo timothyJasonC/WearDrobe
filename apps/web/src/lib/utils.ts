3d6d23d33d656b60ccd6ae914df1ebce0201931c
Izam Ghali <izamghali18@gmail.com>
2024-06-10 11:15:33 +0700

    build(web): added parse thousand func on utils.ts. Added 1 path for route groups on tailwind config

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function parseThousandSeparator(num: number) {
+    let [integerPart, fractionalPart] = num.toString().split('.');
+    let formattedIntegerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    
+    if (fractionalPart) {
+        return `${formattedIntegerPart}.${fractionalPart}`;
+    } else {
+        return formattedIntegerPart;
+    }
+}
+
+export function reverseParseThousandSeparator(formattedStr:string) {
+    let numberStr = formattedStr.replace(/\./g, '');
+    let number = parseFloat(numberStr);
+    
+    return number;
+}
+
+

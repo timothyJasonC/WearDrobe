5804736f5080b36e7f841dbaf89a4bd0a9bd784a
Izam Ghali <izamghali18@gmail.com>
2024-06-19 21:43:53 +0700

    fix: added User type for redux, added catalog route, initial middleware.ts

--- apps/web/src/lib/redux/features/authSlice.ts
+++ apps/web/src/lib/redux/features/authSlice.ts
@@ -1,14 +1,51 @@
+import { IOrder } from "@/constants";
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState: any = {
+enum Gender {
+    MALE = 'MALE',
+    FEMALE = 'FEMALE'
+}
+
+interface IAddressList {
+    id: string;
+    address: string;
+    coordinate: string;
+    userID: string;
+    mainAddress: boolean;
+    user: IUser; // Assuming IUser is already defined
+}
+
+interface IUser {
+    id: string;
+    accountActive: boolean;
+    username?: string | null;
+    email: string;
+    password?: string | null;
+    gender?: Gender | null;
+    dob?: Date | null;
+    createdAt: Date;
+    imgUrl?: string | null;
+    addresses: IAddressList[];
+    orders: IOrder[];
+}
+
+interface CurrentAccount {
+    currentAccount: IUser | null
+}
+
+interface AuthState {
+    value: CurrentAccount
+}
+
+const initialState: AuthState = {
     value: { 
-        currentAccount: {},
+        currentAccount: null,
     }
 }
 
 export const authSlice = createSlice({
     name: 'auth',
-    initialState: initialState,
+    initialState,
     reducers: { 
         setCurrentAccount: (state, action) => {
             return {
@@ -20,7 +57,7 @@ export const authSlice = createSlice({
         clearCurrentAccount: () => {
             return {
                 value: {
-                    currentAccount: {}
+                    currentAccount: null
                 }
             }
         }
@@ -28,4 +65,3 @@ export const authSlice = createSlice({
 })
 
 export const { setCurrentAccount, clearCurrentAccount } = authSlice.actions
-export default authSlice.reducer;
--- apps/web/src/lib/redux/store.ts
+++ apps/web/src/lib/redux/store.ts
@@ -1,13 +1,18 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit";
-import { combineSlices, configureStore } from "@reduxjs/toolkit";
+import { combineReducers, combineSlices, configureStore } from "@reduxjs/toolkit";
 import { cartSlice } from "./features/cart/cartSlice";
+import { authSlice } from "./features/authSlice";
 
-const rootReducer = combineSlices(cartSlice);
+const rootReducer = combineReducers({
+    cart: cartSlice.reducer,
+    auth: authSlice.reducer, // Corrected to use authSlice.reducer
+});
+
 export type RootState = ReturnType<typeof rootReducer>;
 export const makeStore = () => {
-  return configureStore({
-    reducer: rootReducer,
-  });
+    return configureStore({
+        reducer: rootReducer,
+    });
 };
 
 export type AppStore = ReturnType<typeof makeStore>;
